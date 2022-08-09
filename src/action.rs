61233ae218b87046db567c1a45789e37cbc1074e
David O'Rourke <david.orourke@gmail.com>
2022-08-09 13:10:44 +0100

    Add test to action

--- src/action.rs
+++ src/action.rs
@@ -20,3 +20,27 @@ impl From<&Option<String>> for Action {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from() {
+        let tests = vec![
+            (Some("die".to_string()), Action::Die),
+            (Some("start".to_string()), Action::Start),
+            (Some("foobar".to_string()), Action::Other),
+            (None, Action::Other),
+        ];
+
+        for test in tests {
+            let input = test.0;
+            let expected = test.1;
+
+            let action = Action::from(&input);
+
+            assert_eq!(action, expected);
+        }
+    }
+}
